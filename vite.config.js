29214429725db35c0a9770ce7ac764d45f898381
Phan Văn Thành <154895122+thanhphandev@users.noreply.github.com>
2026-01-19 21:30:09 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],,
+    ],
     server: {
         hmr: {
             host: 'travelgo.up.railway.app',
